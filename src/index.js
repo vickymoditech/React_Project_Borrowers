e13dbc0878a0615ad82620af54beb2ce32545a43
vicky <vicky123modi@gmail.com>
2018-03-10 12:48:32 +0530

    reducers structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {applyMiddleware,createStore} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+import promise from 'redux-promise'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers/index';
+
+// step - 1 npm install -save-dev redux-logger
+// step - 2 import logger from 'redux-logger'
+// step - 3 import {applyMiddleware,createStore} from 'redux';
+
+// step - 4
+const middleware = applyMiddleware(promise,logger);
+const store = createStore(reducers,middleware);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
